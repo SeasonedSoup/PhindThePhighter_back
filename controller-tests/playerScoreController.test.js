a55002d676d95aece2ef6d39de536c595b21b7e4
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-14 17:08:41 +0800

    Add more tests in playerScore

--- controller-tests/playerScoreController.test.js
+++ controller-tests/playerScoreController.test.js
@@ -18,5 +18,21 @@ test('Successful Creation of username Bob with score of 102 seconds', done => {
 })
 
 test('Should not be able to create when username is longer than 16 character', done => {
-    request(app).post('/').type('json').send({name: 'Asuperlongnamecontaining16characters', score: 90204}).expect(400, {message: "Name is too long try again"}, done)
+    request(app).post('/').type('json').send({name: 'Asuperlongnamecontaining16characters', score: 90204}).expect(400, {message: "Invalid name created. try again"}, done)
 })
+
+
+test('Should not be able to submit an empty string as a name', done => {
+    request(app).post('/').type('json').send({name: '', score: 1000000}).expect(400, {message: "Invalid name created. try again"}, done)
+})
+
+test('There should be an type error when score is not correctly provided ', done => {
+    request(app).post('/').type('json').send({name: "James", score: ""}).expect(400, {error: "Error creating score"}, done)
+})
+
+test('There should be an type error when name is not correctly provided ', done => {
+    request(app).post('/').type('json').send({name: 1000, score: 50000}).expect(400, {error: "Error creating score"}, done)
+})
+
+
+
--- controllers/playerScoreController.js
+++ controllers/playerScoreController.js
@@ -4,25 +4,23 @@ async function createPlayerScore(req, res) {
     const name = req.body.name
     const score = req.body.score
 
-    if (name.length > 16) {
-        return res.status(400).json({message: "Name is too long try again"});
+    if (name.length > 16 || name.length < 1) {
+        return res.status(400).json({message: "Invalid name created. try again"});
     }
 
     try {
-        const result = await prisma.playerScore.create({
+        await prisma.playerScore.create({
             data: {
                 name: name,
                 timeTakenMs: score,
                 mapId: 1 //tbc
             }
         })
-        if (!result) {
-            return res.status(400).json({message: "Error creating score"});
-        }
 
         return res.status(201).json({message: "Name created successfully"});
     } catch (err) {
         console.error("Error", err)
+        return res.status(400).json({error: "Error creating score"});
     }
     
 }
