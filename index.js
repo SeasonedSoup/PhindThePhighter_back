9ddad0dada92a0d294f2176e8db026402393f602
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-04-27 21:58:10 +0800

    Add Fallback for Port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const validateScoreRouter = require('./routes/validateScoreRoute');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors());
